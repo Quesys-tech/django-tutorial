e089d90f6fd3183f97eaa99a063012034f81cb27
Q M <qdevelop@hotmail.com>
2019-07-23 00:46:49 +0900

    その4完走

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
-from django.http import Http404, HttpResponse
-from django.shortcuts import render,get_object_or_404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -13,13 +15,33 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
-    question=get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/detail.html',{'question':question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
